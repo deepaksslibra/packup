d13e723be5dab13f333abd7229bdf1fc1a723784
Deepak Chandwani <deepaksslibra@gmail.com>
2025-05-17 21:22:30 +0530

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,46 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  js.configs.recommended,
 
-export default eslintConfig;
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended'
+  ),
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      'prettier/prettier': 'warn',
+      'react/jsx-curly-brace-presence': ['warn', 'never'],
+      'react/self-closing-comp': 'warn',
+      'react/jsx-boolean-value': ['warn', 'never'],
+    },
+  },
+
+  {
+    plugins: {
+      tailwindcss: require('eslint-plugin-tailwindcss'),
+      prettier: require('eslint-plugin-prettier'),
+    },
+  },
+
+  prettier,
+];
