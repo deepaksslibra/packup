a0d7fc33bc018762b9a539875c6a6a3ff0059696
Deepak Chandwani <deepaksslibra@gmail.com>
2025-05-18 06:37:16 +0530

    fix build

--- app/trip/[tripId]/page.tsx
+++ app/trip/[tripId]/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import { redirect } from 'next/navigation';
+import { useParams } from 'next/navigation';
 
 /**
  * Main Trip Page - redirects to the edit view by default
  */
-export default async function TripPage({ params }: { params: { tripId: string } }) {
-  const { tripId } = params;
+export default function TripPage() {
+  const params = useParams();
+  const tripId = params.tripId as string;
 
   // Redirect to the edit page by default
   redirect(`/trip/${tripId}/edit`);
